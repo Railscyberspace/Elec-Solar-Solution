34f7d5160347c60e2f74b6600eb46a79af2852c9
Sunday Abam <abamsunday5@gmail.comgit config --edit --globalcodemkdird>
2020-12-18 22:13:55 +0100

    first

---
+++ elecsolar-solutions/src/Components/Form.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class form extends Component
+{
+    render()
+    {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default form
