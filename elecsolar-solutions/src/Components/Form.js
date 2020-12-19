7b136e33a8d933456e74e3f8a95da084452611b5
Sunday Abam <abamsunday5@gmail.comgit config --edit --globalcodemkdird>
2020-12-19 07:50:36 +0100

    psuhing changes

--- elecsolar-solutions/src/Components/Form.js
+++ elecsolar-solutions/src/Components/Form.js
@@ -9,8 +9,9 @@ class form extends Component
         this.state = {
             firstName: '',
             lastName: '',
-            PhoneNumber: '',
+            Phone_Number: '',
             Address: '',
+            Products: '',
             Discription: ''
 
 
@@ -22,6 +23,28 @@ class form extends Component
             firstName: event.target.value
         })
     }
+    handlelastNameChange = event =>
+    {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+
+    handlePhone_NumberChange = event =>
+    {
+        this.setState({
+            Phone_Number: event.target.value
+        })
+    }
+
+
+    handleAddressChange = event =>
+    {
+        this.setState({
+            Address: event.target.value
+        })
+    }
+
 
     handleDiscriptionChange = event =>
     {
@@ -29,33 +52,47 @@ class form extends Component
             Discription: event.target.value
         })
     }
+    handleProductsChange = event =>
+    {
+        this.setState({
+            Products: event.targrt.value
+        })
+    }
+
+    handleSubmit = event =>
+    {
+        alert(`${ this.state.firstName } ${ this.state.lastName } ${ this.state.Phone_Number } ${ this.state.Products } ${ this.state.Address } {$this.Discription}`)
+
+        event.defaultPrevent()
+    }
     render()
     {
+        const { firstName, lastName, Phone_Number, Address, Products, Discription } = this.state
         return (
-            <form>
+            <form onsubmit={this.handleSubmit}>
                 <div>
                     <label>firstName</label>
-                    <input type="text" value={this.state.firstName} onChange={this.handlefirstNameChange} />
+                    <input type="text" value={firstName} onChange={this.handlefirstNameChange} />
                     <label>LastName</label>
-                    <input type="text" />
+                    <input type="text" value={lastName} onChange={this.state.handlelastNameChange} />
                     <labe>Phone Number</labe>
-                    <input type="text" />
+                    <input type="text" value={Phone_Number} onChange={this.state.handlePhone_NumberChange} />
                     <label> Address</label>
-                    <input type="text" />
+                    <input type="text" valuee={Address} onchange={this.state.handleAddressChange} />
                     <div>
                         <label>Discription</label>
-                        <textarea value={this.state.Discription} onChange={this.handleDiscriptionChange} ></textarea>
+                        <textarea value={Discription} onChange={this.handleDiscriptionChange} />
                     </div>
                     <div>
                         <label>Products</label>
-                        <select>
-                            <option value="Inverters"></option>
-                            <option value="Batteries"></option>
-                            <option value="Solar Panels"></option>
+                        <select value={Products} onChange={this.handleProductsChange}>
+                            <option value="Inverters">Inverters</option>
+                            <option value="Batteries">Batteries</option>
+                            <option value="Solar Panels">Solar Panels</option>
                         </select>
                     </div>
-                    <button></button>
-                </div>
+                    <button type="submit">Submit</button>
+                </div >
             </form >
         )
     }
